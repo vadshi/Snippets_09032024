9cdcf9f0d0043d654169d38535ef1afb73342562
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-03-16 11:23:59 +0300

    Add SnippetForm to Snippet

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.core.exceptions import ObjectDoesNotExist
 from MainApp.models import Snippet
 from django.http import HttpResponseNotFound
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -10,8 +11,22 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {"pagename": "Добавление нового сниппета"}
-    return render(request, "pages/add_snippet.html", context)
+    # Создаем пустую форму при запросе методом GET
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+            "pagename": "Добавление нового сниппета",
+            "form": form,
+        }
+        return render(request, "pages/add_snippet.html", context)
+
+    # Получаем данные из формы и на их основе создаем новый snippet в базе
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        return render(request, "add_snippet.html", {"form": form})
 
 
 def snippets_page(request):
@@ -33,3 +48,12 @@ def snippet_detail(request, snippet_id):
         "snippet": snippet,
     }
     return render(request, "pages/snippet_detail.html", context)
+
+
+# def create_snippet(request):
+#     if request.method == "POST":
+#         form = SnippetForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("snippets-list")
+#         return render(request,'add_snippet.html', {'form': form})
