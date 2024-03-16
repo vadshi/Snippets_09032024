35465086a14212ddf3ea1216d04c1875920b8137
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-03-16 10:46:11 +0300

    Done.M5:task11-13

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,17 +1,35 @@
-from django.http import Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from MainApp.models import Snippet
+from django.http import HttpResponseNotFound
 
 
 def index_page(request):
-    context = {'pagename': 'PythonBin'}
-    return render(request, 'pages/index.html', context)
+    context = {"pagename": "PythonBin"}
+    return render(request, "pages/index.html", context)
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    context = {"pagename": "Добавление нового сниппета"}
+    return render(request, "pages/add_snippet.html", context)
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
-    return render(request, 'pages/view_snippets.html', context)
+    snippets = Snippet.objects.all()
+    context = {
+        "pagename": "Просмотр сниппетов",
+        "snippets": snippets,
+    }
+    return render(request, "pages/view_snippets.html", context)
+
+
+def snippet_detail(request, snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Snippet with id={snippet_id} not found")
+    context = {
+        "pagename": "Просмотр сниппета",
+        "snippet": snippet,
+    }
+    return render(request, "pages/snippet_detail.html", context)
