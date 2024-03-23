0ead7035712094c2fd467438d3b276a17de50afd
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-03-23 13:13:08 +0300

    Add order_by

--- MainApp/models.py
+++ MainApp/models.py
@@ -10,6 +10,9 @@ LANGS = (
 
 
 class Snippet(models.Model):
+    class Meta:
+        ordering = ["name", "lang"]
+
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
@@ -18,4 +21,4 @@ class Snippet(models.Model):
     public = models.BooleanField(default=True)  # True = public, False = private
 
     def __repr__(self) -> str:
-        return f'Snippet({self.name})'
+        return f'Snippet({self.name}, {self.lang})'
